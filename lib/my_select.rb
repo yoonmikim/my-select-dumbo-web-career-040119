776c2e05df14ce92f32ec0f2632326245a098f40
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 04:47:55 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ arr = []
+ i = 0 
+ while i < collection.length 
+  if (yield(collection[i]))
+    arr.push((collection[i]))
+  end
+  i += 1
+ end
+ arr
 end
