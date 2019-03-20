33f81101ba3071fc47585100433c002950b7062d
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-20 04:47:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  i = 0 
  while i < collection.length 
   if (yield(collection[i]))
-    arr.push(yield(collection[i]))
+    arr.push((collection[i]))
   end
   i += 1
  end
